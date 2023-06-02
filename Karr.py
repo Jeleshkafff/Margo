f9edd375d7613224903f291fff8ab9a307ef0f94
うきこ <87617334+Jeleshkafff@users.noreply.github.com>
2023-06-02 14:33:26 +0300

    Add files via upload

--- Karr.py
+++ Karr.py
@@ -10,84 +10,154 @@ from pyowm import OWM
 from pyowm.utils.config import get_default_config
 import webbrowser
 import configparser
- 
- 
+from psutil import virtual_memory as memory
+import nltk
+import json
+
+
+with open('NEW_BIG_BOT_CONFIG.json', 'r') as f:
+    BOT_CONFIG = json.load(f)
+
+
 class Assistant:
- 
     settings = configparser.ConfigParser()
     settings.read('settings.ini')
- 
+
     config_dict = get_default_config()  # Инициализация get_default_config()
     config_dict['language'] = 'ru'  # Установка языка
- 
+
     def __init__(self):
         self.engine = pyttsx3.init()
         self.r = sr.Recognizer()
         self.text = ''
- 
+
         self.cmds = {
-            ('текущее время', 'сейчас времени', 'который час',"время"): self.time,
+            ('текущее время', 'сейчас времени', 'который час'): self.time,
             ('привет', 'добрый день', 'здравствуй'): self.hello,
-            ('пока', 'прощай'): self.quite,
-            ('марго', 'маргошка', 'солнце', 'солнышко', 'моё солнышко',"солнце мое"): self.name,
+            ('пока', 'вырубись'): self.quite,
+            ('добавить задачу', 'добавить заметку', 'создай заметку', 'создай задачу'): self.task_planner,
+            ('список задач', 'список заметок', 'задачи', 'заметки'): self.task_list,  
+            ('загруженость компьютера', 'загруженость системы', 'загруженость',
+             'состояние системы', 'какая загрузка системы', 'какая загрузка'): self.check_memory,
+            ('марго', 'маргошка', 'солнце', 'солнышко', 'моё солнышко',"солнце мое"): self.name, 
         }
- 
+
         self.ndels = ['ладно', 'не могла бы ты', 'пожалуйста',
                       'текущее', 'сейчас']
- 
+
         self.commands = [
-            'текущее время', 'сейчас времени' ,"время", 'который час',
+            'текущее время', 'сейчас времени', 'который час',
             'открой браузер', 'открой интернет', 'запусти браузер',
             'привет', 'добрый день', 'здравствуй',
-            'пока', 'прощай',
+            'пока', 'вырубись',
+            'добавить задачу', 'добавить заметку', 'создай заметку', 'создай задачу'
+            'список задач', 'список заметок', 'задачи', 'заметки',
+            'загруженость компьютера', 'загруженость системы', 'какая загрузка'
         ]
- 
+
         self.num_task = 0
         self.j = 0
         self.ans = ''
- 
+
     def cleaner(self, text):
         self.text = text
- 
+
         for i in self.ndels:
             self.text = self.text.replace(i, '').strip()
             self.text = self.text.replace('  ', ' ').strip()
- 
+
         self.ans = self.text
- 
+
         for i in range(len(self.commands)):
             k = fuzz.ratio(text, self.commands[i])
             if (k > 70) & (k > self.j):
                 self.ans = self.commands[i]
                 self.j = k
- 
+
         return str(self.ans)
- 
+
+    def check_memory(self):
+        mem = memory()
+        self.talk(f'Компьютер загружен на {round(mem.percent)}%')
+
+    def intent_cleaner(self, text):
+        cleaned_text = ''
+        for i in text.lower():
+            if i in 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяabcdefghijklmnopqrstuvwxyz ':
+                cleaned_text += i
+        return cleaned_text
+
+    def match(self, text, example):
+        return nltk.edit_distance(text, example) / len(example) < 0.4 if len(example) > 0 else False
+
+    def get_intent(self, text):
+        for intent in BOT_CONFIG['intents']:
+            if 'examples' in BOT_CONFIG['intents'][intent]:
+                for example in BOT_CONFIG['intents'][intent]['examples']:
+                    if self.match(self.intent_cleaner(text), self.intent_cleaner(example)):
+                        return intent
+
+    def intenter(self, text):
+        intent = self.get_intent(text)
+
+        if intent is None:
+            return
+
+        self.talk(choice(BOT_CONFIG['intents'][intent]['responses']))
+
+    def task_planner(self):
+        self.talk("Что добавить в список задач?")
+        task = self.listen()
+
+        with open('TODO_LIST.txt', 'a') as file:
+            file.write(f'{task}\n')
+
+        self.talk(f'Задача {task} добавлена в список задач!')
+
+    def task_list(self):
+        with open('TODO_LIST.txt', 'r') as file:
+            tasks = file.read()
+        self.talk(f"Список задач:\n{tasks}")
+
+    def web_search(self, search):
+        words = ['найди', 'найти', 'ищи', 'кто такой', 'что такое']
+        remove = ["пожалуйста", "ладно", "давай", "сейчас"]
+        for i in words:
+            search = search.replace(i, '')
+            for j in remove:
+                search = search.replace(j, '')
+                search = search.strip()
+        self.talk(f"Ищу {search}")
+        webbrowser.open(f'https://www.google.com/search?q={search}&oq={search}'
+                        f'81&aqs=chrome..69i57j46i131i433j0l5.2567j0j7&sourceid=chrome&ie=UTF-8')
+
     def recognizer(self):
         self.text = self.cleaner(self.listen())
         print(self.text)
- 
-        if self.text.startswith(('открой', 'запусти', 'зайди', 'зайди на', "включи")):
+
+        if self.text.startswith(('открой', 'запусти', 'зайди', 'зайди на')):
             self.opener(self.text)
- 
+        ####
+        elif self.text.startswith(('найди', 'найти', 'ищи', 'кто такой', 'что такое')):
+            self.web_search(self.text)
+
         for tasks in self.cmds:
             for task in tasks:
                 if fuzz.ratio(task, self.text) >= 80:
                     self.cmds[tasks]()
- 
-        self.engine.runAndWait()
-        self.engine.stop()
- 
+                    return
+
+        self.intenter(self.text)
+
     def time(self):
         now = datetime.datetime.now()
-        self.talk("Сейчас " + str(now.hour) + ":" + str(now.minute))
- 
+        self.talk(f"Сейчас {now.hour} : {now.minute}")
+
     def opener(self, task):
         links = {
             ('youtube', 'ютуб', 'ютюб'): 'https://youtube.com/',
             ('вк', 'вконтакте', 'контакт', 'vk'): 'https:vk.com/feed',
             ('браузер', 'интернет', 'browser'): 'https://google.com/',
-            
         }
         j = 0
         if 'и' in task:
@@ -102,42 +172,44 @@ class Assistant:
                             self.talk('Открываю ' + double_task[i])
                             j += 1
                             break
- 
-    def cfile(self):
-        try:
-            cfr = Assistant.settings['SETTINGS']['fr']
-            if cfr != 1:
-                file = open('settings.ini', 'w', encoding='UTF-8')
-                file.write('[SETTINGS]\ncountry = UA\nplace = Kharkov\nfr = 1')
-                file.close()
-        except Exception as e:
-            print('Перезапустите Ассистента!', e)
-            file = open('settings.ini', 'w', encoding='UTF-8')
-            file.write('[SETTINGS]\ncountry = UA\nplace = Kharkov\nfr = 1')
-            file.close()
- 
+
+
     def quite(self):
         self.talk(choice(['Надеюсь мы скоро увидимся', 'Рада была помочь', 'Пока пока', 'Я отключаюсь']))
         self.engine.stop()
         system('cls')
         sys.exit(0)
- 
-   
- 
+
+    def shut(self):
+        self.talk("Подтвердите действие!")
+        text = self.listen()
+        print(text)
+        if (fuzz.ratio(text, 'подтвердить') > 60) or (fuzz.ratio(text, "подтверждаю") > 60):
+            self.talk('Действие подтверждено')
+            self.talk('До скорых встреч!')
+            system('shutdown /s /f /t 10')
+            self.quite()
+        elif fuzz.ratio(text, 'отмена') > 60:
+            self.talk("Действие не подтверждено")
+        else:
+            self.talk("Действие не подтверждено")
+
     def hello(self):
         self.talk(choice(['Привет, чем могу помочь?', 'Здраствуйте', 'Приветствую']))
     def name(self):
         self.talk(choice(['Да, я', 'Что?', 'Хи-хи, что хотите?',"м? котя"]))
  
-   
+
     def talk(self, text):
         print(text)
         self.engine.say(text)
         self.engine.runAndWait()
- 
+        ####
+        self.engine.stop()
+
     def listen(self):
         with sr.Microphone() as source:
-            print(colorama.Fore.LIGHTGREEN_EX + "Я вас слушаю...")
+            print(f"{colorama.Fore.LIGHTGREEN_EX}Я вас слушаю...")
             self.r.adjust_for_ambient_noise(source)
             audio = self.r.listen(source)
             try:
@@ -145,10 +217,12 @@ class Assistant:
             except Exception as e:
                 print(e)
             return self.text
- 
- 
-Assistant().cfile()
- 
- 
+
+
+# Assistant().cfile()
+
 while True:
-    Assistant().recognizer()
+    try:
+        Assistant().recognizer()
+    except Exception as ex:
+        print(ex)
